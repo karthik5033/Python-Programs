71a67ed16106d85158e2e86ffb89f8bdf142f893
karthik5033 <karthikkp5033@gmail.com>
2025-03-14 23:53:44 +0530

    added new codes

---
+++ python/python1.py
@@ -0,0 +1,13 @@
+light=input("please provide a light color : ")
+
+if(light=="red"):
+    print("stop")
+
+elif(light=="yellow"):
+    print("look")
+elif(light=="green"):
+    print("go")
+else:
+    print("light is broken")
+
+
---
+++ python/python2.py
@@ -0,0 +1,16 @@
+marks = int(input("enter marks : "))
+
+if(marks>=90):
+  grade = "A"
+elif(marks>=80 and marks < 90):
+    grade = "B"
+elif(marks>=70 and marks < 80):
+     grade = "C"
+else:
+    grade ="D"
+print("grade obtained is ",grade)
+
+
+
+
+
